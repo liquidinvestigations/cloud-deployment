7493ce0eab06e84fe5188735e5570ac5d8e5451c
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-10-28 17:51:45 +0300

    fix script

--- launch_web_ui.sh
+++ launch_web_ui.sh
@@ -5,6 +5,6 @@ factory/factory --platform liquid-cloud-x86_64 \
 	--memory 2048 \
 	--share guest-scripts:/mnt/scripts \
 	--share web-ui:/opt/web-ui \
-	--tcp 10080:80 \  # http
-	--tcp 10022:22 \  # ssh
+	--tcp 10080:80 \
+	--tcp 10022:22 \
 	/mnt/scripts/liquid_vm_boot.sh
