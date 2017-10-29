8751ef8efe54f6c36c93da037bc3185027b10155
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-10-29 18:01:02 +0200

    add launch_cloud script

--- launch_cloud.sh
+++ launch_cloud.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -ex
-factory/factory --platform cloud-x86_64 login --smp 4 --memory 2048 --share .:/mnt/shared
+factory/factory --platform cloud-x86_64 \
+	login \
+	--smp 4 \
+	--memory 4096 \
+	--share guest-scripts:/mnt/scripts \
+	--share web-ui:/opt/web-ui \
+	--tcp 10080:80 \
+	--tcp 10022:22
