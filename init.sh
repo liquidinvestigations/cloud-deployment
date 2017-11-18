b5a0fac638ab3bc7f48d50dbdce100a9f3378f01
nic <nicolae.mihale@gmail.com>
2017-11-18 12:11:14 +0200

    add ui-templates to the shared folder list

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 FACTORY_REPO=https://github.com/liquidinvestigations/factory.git
+WEB_UI_REPO=https://github.com/liquidinvestigations/web-ui.git
+UI_TEMPLATES_REPO=https://github.com/liquidinvestigations/ui-templates.git
 
 PROGRAM_LIST="git curl wget kitchen qemu-system-x86_64"
 for program in $PROGRAM_LIST; do
@@ -16,3 +18,14 @@ if [ ! -d "factory" ]; then
 fi
 
 ( cd factory; git checkout master; git pull )
+if [ ! -d "ui-templates" ]; then
+	git clone $UI_TEMPLATES_REPO
+fi
+
+if [ ! -d "web-ui" ]; then
+	git clone $WEB_UI_REPO
+fi
+
+( cd factory; git checkout master; git pull )
+( cd web-ui; git checkout master; git pull )
+( cd ui-templates; git checkout master; git pull )
--- launch_web_ui.sh
+++ launch_web_ui.sh
@@ -12,6 +12,7 @@ factory/factory --platform liquid-cloud-x86_64 \
 	--memory 4096 \
 	--share guest-scripts:/mnt/scripts \
 	--share web-ui:/opt/web-ui \
+	--share ui-templates:/opt/ui-templates \
 	--tcp 10080:80 \
 	--tcp 10022:22 \
 	/mnt/scripts/liquid_vm_boot.sh
