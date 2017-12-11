87788545d9e045ab9edb299fdca8fb9419094df7
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-12-11 16:57:08 +0200

    update archive format from xz to gz; fix ssh user

--- download.sh
+++ download.sh
@@ -2,24 +2,24 @@
 
 ./init.sh
 
-UBUNTU_CLOUD=https://jenkins.liquiddemo.org/job/liquidinvestigations/job/factory/job/master/lastSuccessfulBuild/artifact/cloud-x86_64-image.tar.xz
-LIQUID_CLOUD=https://jenkins.liquiddemo.org/job/liquidinvestigations/job/setup/view/change-requests/job/master/lastSuccessfulBuild/artifact/liquid-cloud-x86_64-raw.img.xz
+UBUNTU_CLOUD=https://jenkins.liquiddemo.org/job/liquidinvestigations/job/factory/job/master/lastSuccessfulBuild/artifact/cloud-x86_64-image.tar.gz
+LIQUID_CLOUD=https://jenkins.liquiddemo.org/job/liquidinvestigations/job/setup/view/change-requests/job/master/lastSuccessfulBuild/artifact/liquid-cloud-x86_64-raw.img.gz
 
 mkdir -pv factory/images/cloud-x86_64
 (
   cd factory/images/cloud-x86_64
-  curl -L $UBUNTU_CLOUD > disk.tar.xz
-  xzcat disk.tar.xz | tar -x
-  rm disk.tar.xz
+  curl -L $UBUNTU_CLOUD > disk.tar.gz
+  zcat disk.tar.gz | tar -x
+  rm disk.tar.gz
 )
 
 mkdir -pv factory/images/liquid-cloud-x86_64
 (
   cd factory/images/liquid-cloud-x86_64
-  curl -L $LIQUID_CLOUD > disk.img.xz
-  xzcat disk.img.xz > disk.img
-  rm disk.img.xz
-  echo '{"login": {"username": "liquid", "password": "liquid"}}' > config.json
+  curl -L $LIQUID_CLOUD > disk.img.gz
+  zcat disk.img.gz > disk.img
+  rm disk.img.gz
+  echo '{"login": {"username": "liquid-admin", "password": "liquid"}}' > config.json
 )
 
 factory/factory run --share setup:/mnt/setup --share factory/images/liquid-cloud-x86_64:/mnt/liquid /mnt/setup/bin/with-image-chroot /mnt/liquid/disk.img bash /opt/setup/ci/prepare-image-for-testing
