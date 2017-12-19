f15c66e899e09e41d5f2321cef0254cc658711c9
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-12-19 16:47:04 +0200

    mount setup and run tests from /opt/setup

--- launch_tests.sh
+++ launch_tests.sh
@@ -4,7 +4,7 @@ factory/factory --platform liquid-cloud-x86_64 \
 	--smp 4 \
 	--memory 4096 \
 	--share guest-scripts:/mnt/scripts \
-	--share ../repos/setup:/mnt/setup \
+	--share ../repos/setup:/opt/setup \
 	--tcp 10080:80 \
 	--tcp 10022:22 \
 	/mnt/scripts/liquid_vm_test.sh
