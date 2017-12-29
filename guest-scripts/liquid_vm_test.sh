15f88e56384f4853316534b6ebc833dc81ff2edb
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-12-29 13:31:24 +0200

    don't kill the VM when the tests fail

--- guest-scripts/liquid_vm_test.sh
+++ guest-scripts/liquid_vm_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-PYTHONUNBUFFERED=yyy /opt/setup/bin/run_first_boot_tests.py
+PYTHONUNBUFFERED=yyy /opt/setup/bin/run_first_boot_tests.py || echo "tests failed"
 echo "Press CTRL-C to kill the VM."
 sleep infinity
