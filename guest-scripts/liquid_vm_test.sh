689c4490bbf3ba0883e1536a8aab792884d1dbf7
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2018-03-29 15:29:16 +0300

    test like jenkins

--- guest-scripts/liquid_vm_test.sh
+++ guest-scripts/liquid_vm_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-PYTHONUNBUFFERED=yyy /opt/setup/bin/run_first_boot_tests.py || echo "tests failed"
-echo "Press CTRL-C to kill the VM."
-sleep infinity
+PYTHONUNBUFFERED=yyy /opt/setup/bin/run_first_boot_tests.py || ( echo "liquid_vm_test: tests failed" && exit 1 )
+#echo "Press CTRL-C to kill the VM."
+#sleep infinity
--- launch_cloud.sh
+++ launch_cloud.sh
@@ -4,7 +4,5 @@ factory/factory login \
 	--smp 4 \
 	--memory 4096 \
 	--share guest-scripts:/mnt/scripts \
-	--share web-ui:/opt/web-ui \
-	--tcp 10080:80 \
-	--tcp 10022:22 \
+	--share setup:/opt/setup \
 	"$@"
